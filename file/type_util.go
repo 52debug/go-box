5e03bb7cf8b2403004d78974b8efe75a277fe530
mky <9717790@qq.com>
2025-12-30 12:41:30 +0800

    add

---
+++ file/type_util.go
@@ -0,0 +1,25 @@
+package file
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// IsJSONFile 判断是否为 JSON 文件
+func IsJSONFile(filename string) bool {
+	ext := strings.ToLower(filepath.Ext(filename))
+	return ext == ".json"
+}
+
+// IsFile 判断是否为文件（非目录）
+func IsFile(path string) bool {
+	info, err := os.Stat(path)
+	return err == nil && !info.IsDir()
+}
+
+// IsDir 判断是否为目录（非文件）
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	return err == nil && info.IsDir()
+}
